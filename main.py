f410271a690dd086b30e283743e2be0cd4139a7d
tibynx <tibynx@protonmail.com>
2026-01-09 20:51:06 +0100

    Added logging setup and updated bot initialization

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import discord
 from discord.ext import commands
@@ -6,10 +7,27 @@ from config import BOT_TOKEN, SYNC_GUILD
 # Set intents
 intents = discord.Intents.default()
 
+# Set up logging
+logs_dir = os.path.join(os.path.dirname(__file__), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+timestamp = discord.utils.utcnow().strftime("%Y-%m-%d_%H-%M-%S") # UTC timestamp
+log_filename = os.path.join(logs_dir, f"log_{timestamp}.log")
+
+logger = logging.getLogger("discord.app")
+logger.setLevel(logging.INFO)
+log_handler = logging.FileHandler(filename=log_filename, encoding="utf-8", mode="w")
+# Using Python's standard formatting style
+LOG_FORMAT = "[{asctime}] [{levelname:<8}] {name}: {message}"
+log_formatter = logging.Formatter(LOG_FORMAT, "%Y-%m-%d %H:%M:%S", style="{")
+log_handler.setFormatter(log_formatter)
+logger.addHandler(log_handler)
+
+
 class DiscordBot(commands.Bot):
     def __init__(self) -> None:
         # No prefix since we use app commands
         super().__init__(command_prefix="", intents=intents)
+        self.logger = logger
 
     # Load cogs
     async def load_cogs(self) -> None:
@@ -18,32 +36,37 @@ class DiscordBot(commands.Bot):
                 extension = file[:-3]
                 try:
                     await self.load_extension(f"cogs.{extension}")
-                    print(f"Loaded extension '{extension}'")
+                    self.logger.info("Loaded extension '%s'", extension)
                 except Exception as error:
-                    print(f"Failed to load extension '{extension}'")
-                    print(error)
-
+                    self.logger.error(
+                        "Failed to load extension '%s': %s", extension, type(error).__name__
+                    )
+                    self.logger.exception(error)
 
     async def setup_hook(self) -> None:
-        print(f"Logged in as {self.user.name}#{self.user.discriminator} (User ID: {self.user.id})")
-        print(f"discord.py version: {discord.__version__}")
-        print("-------------------")
+        self.logger.info(
+            "Logged in as %s#%s (ID: %s)", self.user.name, self.user.discriminator, self.user.id
+        )
+        self.logger.info("discord.py version: %s", discord.__version__)
+        self.logger.info("-------------------")
         await self.load_cogs()
 
         # Sync global commands
         try:
             synced_global = await self.tree.sync()
-            print(f"Synced {len(synced_global)} global commands.")
-        except Exception as e:
-            print(f"Failed to sync global commands: {e}")
+            self.logger.info("Synced %d global interactions", len(synced_global))
+        except Exception as error:
+            self.logger.error("Failed to sync global interaction: %s", type(error).__name__)
+            self.logger.exception(error)
 
         # Sync specific guild commands
         guild = discord.Object(id=SYNC_GUILD)
         try:
             synced_guild = await self.tree.sync(guild=guild)
-            print(f"Synced {len(synced_guild)} commands to Guild ID {guild.id}.")
-        except Exception as e:
-            print(f"Failed to sync guild commands: {e}")
+            self.logger.info("Synced %d guild interactions to Guild ID %s", len(synced_guild), guild.id)
+        except Exception as error:
+            self.logger.error("Failed to sync guild interaction: %s", type(error).__name__)
+            self.logger.exception(error)
 
 
 # Run the bot
